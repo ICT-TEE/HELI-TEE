9bf1c6939881d7b2e19931489076493a2e04ac0a
Gwins7 <gwins777@qq.com>
2024-05-16 14:50:25 +0800

    env: update env.sh and submodule

---
+++ env.sh
@@ -0,0 +1,6 @@
+# below homes is submodules of this repo
+export SBI_HOME=$(pwd)/opensbi
+# export LINUX_HOME=$(shell pwd)/riscv-linux
+export LINUX_HOME=$(pwd)/debian-linux-kernel
+export RISCV_ROOTFS_HOME=$(pwd)/riscv-rootfs
+export NEMU_HOME=$(pwd)/NEMU
